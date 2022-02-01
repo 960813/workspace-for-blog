f5d28bc3a077e83eba64a504dc583b7400c68b3b
Taeyang Jin <sun@jupiterflow.com>
2022-02-01 16:03:29 +0900

    Effective Kotlin item2 - P024

---
+++ src/main/kotlin/effective/item02/P024.kt
@@ -0,0 +1,33 @@
+package effective.item02
+
+/**
+ * @Author Heli
+ */
+
+fun primeV1() {
+    var numbers = (2..100).toList()
+    val primes = mutableListOf<Int>()
+    while (numbers.isNotEmpty()) {
+        val prime = numbers.first()
+        primes.add(prime)
+        numbers = numbers.filter { it % prime != 0 }
+    }
+    println(primes)
+}
+
+val primes: Sequence<Int> = sequence {
+    var numbers = generateSequence(2) { it + 1 }
+
+    while (true) {
+        val prime = numbers.first()
+        yield(prime)
+        numbers = numbers.drop(1)
+            .filter { it % prime != 0 }
+    }
+}
+
+fun main() {
+
+    primeV1()
+    println(primes.take(25).toList())
+}
